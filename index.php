11860999dd6c1d321d3238ec82cc7ecf548b87b6
Robe Zhang <rzhang8277@bths.edu>
2017-02-17 22:28:39 -0500

    php

--- index.php
+++ index.php
@@ -5,22 +5,24 @@
     </head>
     <body>
     
-    <h1>Hi Socket!</h1>
+    <h1>Welcome to <img src = "blank.png"/> </h1>
 	<div id = "maindiv">
 		<div id = "logintab">
 			<div>Login</div>
 			<form id = "login">
 				<input name = "email" placeholder = "Email" type = "text" /> <br>
-				<input name = "password" placeholder = "Password" type = "password" />
-			</form>
+				<input name = "password" placeholder = "Password" type = "password" /> <br>
+                <input type="submit" value="Submit">
+            </form>
 		</div>
 		<div id = "registertab">
 			<div>Register</div>
 			<form id = "register">
-				<input name = "username" placeholder = "username" type = "text" /><br>
+				<input name = "username" placeholder = "username" type = "text" /> <br>
 				<input name = "email" placeholder = "Email" type = "text" /> <br>
-				<input name = "password" placeholder = "Password" type = "password" />
-			</form>
+				<input name = "password" placeholder = "Password" type = "password" /> <br>
+                <input type="submit" value="Submit">
+            </form>
 		</div>
 	</div>
 
