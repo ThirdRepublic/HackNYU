23b78e72be1a2e455edba23e168a51232d71dee0
ntc27 <ntc27@cornell.edu>
2017-02-17 23:36:33 -0500

    added logo file

---
+++ logo/logo.php
@@ -0,0 +1,8 @@
+<svg id = "logo" width = "400" height = "200" >
+	<text fill = "black" x = "10" y = "49" transform = "scale(2 2)">save</text>
+	<text class = "Aplus" fill = "red" x = "43.5" y = "50" transform = "scale(2 2)">A</text>
+	<text class = "Aplus" fill = "red" x = "66" y = "54" transform = "scale(1.6 1.6)">+</text>
+	<text fill = "black" x = "59" y = "49" transform = "scale(2 2)">grade</text>
+	<line transform = "scale(2 2)" stroke-width = "1.2" stroke = "black" x1 = "9" x2 = "59" y1 = "51" y2 = "51"/>
+	<line transform = "scale(2 2)" stroke-width = "1.2" stroke = "black" x1 = "68" x2 = "103" y1 = "51" y2 = "51"/>		
+</svg>
---
+++ logo/logocss.php
@@ -0,0 +1,9 @@
+<link href="https://fonts.googleapis.com/css?family=ABeeZee|Open+Sans" rel="stylesheet">
+<style>
+	text{
+		font-family: 'ABeeZee', sans-serif;
+	}
+	.Aplus{
+		font-size: 17pt;
+	}
+</style>
