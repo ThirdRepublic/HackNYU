ade7aa392a8b58e6854279a56880638c2d9854ac
ntc27 <ntc27@cornell.edu>
2017-02-17 21:17:46 -0500

    changed index

--- index.php
+++ index.php
@@ -6,6 +6,23 @@
     <body>
     
     <h1>Hi Socket!</h1>
+	<div id = "maindiv">
+		<div id = "logintab">
+			<div>Login</div>
+			<form id = "login">
+				<input name = "email" placeholder = "Email" type = "text" /> <br>
+				<input name = "password" placeholder = "Password" type = "password" />
+			</form>
+		</div>
+		<div id = "registertab">
+			<div>Register</div>
+			<form id = "register">
+				<input name = "username" placeholder = "username" type = "text" /><br>
+				<input name = "email" placeholder = "Email" type = "text" /> <br>
+				<input name = "password" placeholder = "Password" type = "password" />
+			</form>
+		</div>
+	</div>
 
     </body>
 </html>
