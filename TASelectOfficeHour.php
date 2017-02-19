9ee88c20b9a4e82b05a0db28d08eeeba93fd7257
Robe Zhang <rzhang8277@bths.edu>
2017-02-18 23:41:26 -0500

    Office hours display

--- TASelectOfficeHour.php
+++ TASelectOfficeHour.php
@@ -2,3 +2,26 @@
     session_start();
     $conn = new PDO("mysql:host=localhost;dbname=hacknyu", "root", "");
     
+    
+    /*
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Office Hour</title>
+		<?php include "include/headerscript.php" ?>
+        <style>
+           
+        </style>
+	</head>
+	<body>
+        <div id = "mainbody" class = "container well">
+            <?php 
+                include "include/header.php";
+                session_start();
+                $conn = new PDO("mysql:host=localhost;dbname=hacknyu", "root", "");
+                
+            ?>
+        </div>
+    </body>
+</html>
+*/
