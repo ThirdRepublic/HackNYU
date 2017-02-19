f4548cfaecde6065c94cd9b079a1c067172cd11f
Robe Zhang <rzhang8277@bths.edu>
2017-02-19 11:57:37 -0500

    d

--- include/header.php
+++ include/header.php
@@ -19,18 +19,34 @@
   		</div>
 	</nav>
 -->
-
+    <?php
+        session_start();
+        $conn = new PDO("mysql:host=localhost;dbname=hacknyu", "root", "");
+        $statement = $conn->prepare("SELECT IsStudent, IsTA FROM users WHERE email = '$_SESSION[email]'");
+        $statement->execute();
+        $result = $statement->fetch();
+        $isTA = $result['IsTA'];
+        $isStudent = $result['IsStudent'];
+        $link = "";
+        if(!$isStudent AND !$isTA)
+            $link = "professorAddTA.php";
+        else
+            if($isTA)
+                $link = "TASelectOfficeHour.php";
+            else
+                $link = "StudentSelectClasses.php";
+    ?>
 	<div id = "navbar" class = "navbar navbar-fixed-top">
-		<span href = "myclasses.php"><div id = "logocontainer" class = "logobox"><?php include "logo/logo.php" ?></div></span>
+		<a href = "myclasses.php" <div id = "logo"><?php include "logo/logo.php" ?></div> </a>
 		<div class = "navcontainer">
-			<a class = "navbar-a" href = "officehour.php">Office Hour</a>	<!--	add a drop tab there for prof and ta -->
-			<a class = "navbar-a" href = "myclasses.php">My Classes</a>
-			<a class = "navbar-a" href = "help.php">Help</a>
-			<a class = "navbar-a" id = "profile">Profile</a><br>
+			<a href = "officehour.php">Office Hour</a>	<!--	add a drop tab there for prof and ta -->
+			<a href = "myclasses.php">My Classes</a>
+			<a href = "help.php">Help</a>
+			<a id = "profile">Profile</a><br>
 			<div id = "subprofile">
 				<div><img alt = "pfp"></div>
-				<div><a class = "navbar-a" href = "setting.php">Setting</a></div>
-				<div><a class = "navbar-a" href = "logOut.php">Logout</a></div>
+				<div><a href = "setting.php">Setting</a></div>
+				<div><a href = "logOut.php">Logout</a></div>
 			</div>
 		</div>
 	</div>
