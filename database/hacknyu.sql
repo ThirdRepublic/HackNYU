9c3194b2fce5491715912ced54c4e499f8aa7451
Nicholas Pena <np1514@nyu.edu>
2017-02-19 10:42:48 -0500

    morning pain

--- database/hacknyu.sql
+++ database/hacknyu.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2017 at 04:07 PM
+-- Generation Time: Feb 19, 2017 at 04:42 PM
 -- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `appointment` (
   `app_ID` int(255) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `duration` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `duration` int(4) NOT NULL,
   `queue` int(255) NOT NULL,
   `categories` varchar(500) NOT NULL,
   `question` varchar(500) NOT NULL,
@@ -42,11 +42,11 @@ CREATE TABLE `appointment` (
 --
 
 INSERT INTO `appointment` (`app_ID`, `timestamp`, `duration`, `queue`, `categories`, `question`, `oh_ID`, `email`) VALUES
-(101, '2017-02-19 09:30:03', '2016-07-12 07:15:00', 1, 'Math', '6', 501, 'bob@bob.bob'),
-(102, '2017-02-19 09:30:17', '2016-11-02 14:30:00', 5, 'English', '34', 502, 'bob@bob.bob'),
-(103, '2017-02-19 09:32:16', '2016-11-02 17:45:00', 20, 'Functional Programming', '100', 502, 'bill@bill.bill'),
-(104, '2017-02-19 09:32:29', '0000-00-00 00:00:00', 3, 'Computer Architecture', '7', 503, 'ken@ken.ken'),
-(105, '2017-02-19 09:32:53', '0000-00-00 00:00:00', 2, 'Algorithms', '4', 501, 'bob@bob.bob');
+(101, '2017-02-19 15:41:08', 20, 1, 'Math', '6', 501, 'bob@bob.bob'),
+(102, '2017-02-19 15:41:11', 20, 5, 'Physics', '34', 502, 'bob@bob.bob'),
+(103, '2017-02-19 15:41:14', 20, 20, 'Computer Science', '21', 502, 'bill@bill.bill'),
+(104, '2017-02-19 15:41:17', 20, 3, 'Computer Science', '7', 503, 'juice@juice.com'),
+(105, '2017-02-19 15:41:20', 20, 2, 'Math', '4', 501, 'bob@bob.bob');
 
 -- --------------------------------------------------------
 
@@ -55,21 +55,22 @@ INSERT INTO `appointment` (`app_ID`, `timestamp`, `duration`, `queue`, `categori
 --
 
 CREATE TABLE `bookmarked` (
-  `email` varchar(200) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
   `class_ID` int(255) DEFAULT NULL,
-  `isEnrolled` tinyint(1) DEFAULT NULL
+  `isEnrolled` tinyint(1) DEFAULT NULL,
+  `id` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bookmarked`
 --
 
-INSERT INTO `bookmarked` (`email`, `class_ID`, `isEnrolled`) VALUES
-('test@gmail.com', 12345, 1),
-('tea@gmail.com', 54321, 0),
-('bob@bob.bob', 201, 1),
-('bill@bill.bill', 203, 0),
-('ken@ken.ken', 205, 1);
+INSERT INTO `bookmarked` (`email`, `class_ID`, `isEnrolled`, `id`) VALUES
+('bob@bob.bob', 1003, 1, 1),
+('bob@bob.bob', 4513, 0, 2),
+('juice@juice.com', 2033, 1, 3),
+('juice@juice.com', 2413, 1, 4),
+('juice@juice.com', 2224, 1, 5);
 
 -- --------------------------------------------------------
 
@@ -79,6 +80,7 @@ INSERT INTO `bookmarked` (`email`, `class_ID`, `isEnrolled`) VALUES
 
 CREATE TABLE `classes` (
   `class_ID` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `description` varchar(500) DEFAULT NULL,
   `Start` date NOT NULL,
   `End` date NOT NULL
@@ -88,12 +90,12 @@ CREATE TABLE `classes` (
 -- Dumping data for table `classes`
 --
 
-INSERT INTO `classes` (`class_ID`, `description`, `Start`, `End`) VALUES
-(201, 'Suffering -Computer Edition', '0000-00-00', '0000-00-00'),
-(202, 'Suffering - Science Edition', '0000-00-00', '0000-00-00'),
-(203, 'Suffering - English Edition', '0000-00-00', '0000-00-00'),
-(205, 'Suffering - Discrete Math Edition', '0000-00-00', '0000-00-00'),
-(12345, 'Suffering - Ultimate Edition', '0000-00-00', '0000-00-00');
+INSERT INTO `classes` (`class_ID`, `name`, `description`, `Start`, `End`) VALUES
+(1003, 'General Engineering', '', '2017-01-23', '2017-05-08'),
+(2033, 'Waves, Optics and Thermodynamics', '', '2017-01-23', '2017-05-08'),
+(2224, 'Data Analysis', '', '2017-01-23', '2017-05-08'),
+(2413, 'Discrete Mathematics', '', '2017-01-23', '2017-05-08'),
+(4513, 'Software Engineering', '', '2017-01-23', '2017-05-08');
 
 -- --------------------------------------------------------
 
@@ -119,24 +121,6 @@ INSERT INTO `office_hour` (`oh_ID`, `class_ID`, `staff_ID`, `time_Begin`, `locat
 (502, 205, '404', '17:00:00', 'Room 4', '02:00:00'),
 (503, 203, '402', '12:00:00', 'Room 15', '02:30:00'),
 (505, 205, '402', '10:00:00', 'Room 11', '02:00:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `staff`
---
-
-CREATE TABLE `staff` (
-  `email` varchar(200) NOT NULL,
-  `class_ID` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
---
--- Dumping data for table `staff`
---
-
-INSERT INTO `staff` (`email`, `class_ID`) VALUES
-('time@gmail.com', 201);
 
 -- --------------------------------------------------------
 
@@ -180,7 +164,7 @@ ALTER TABLE `appointment`
 -- Indexes for table `bookmarked`
 --
 ALTER TABLE `bookmarked`
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Indexes for table `classes`
@@ -194,12 +178,6 @@ ALTER TABLE `classes` ADD FULLTEXT KEY `description` (`description`);
 --
 ALTER TABLE `office_hour`
   ADD UNIQUE KEY `oh_ID` (`oh_ID`);
-
---
--- Indexes for table `staff`
---
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`email`,`class_ID`);
 
 --
 -- Indexes for table `users`
