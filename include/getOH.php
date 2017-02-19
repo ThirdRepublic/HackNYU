3f40cc90a343a7c5f3c326e301d73b310c0ef827
ntc27 <ntc27@cornell.edu>
2017-02-19 03:23:14 -0500

    eh

--- include/getOH.php
+++ include/getOH.php
@@ -17,12 +17,12 @@ function is_ajax() {
 }
 
 function test_function(){*/
-	$cmd = "SELECT oh_ID, class_ID, time_Begin, location FROM office_hour WHERE class_ID = ".$_POST['select'];
+	$cmd = "SELECT o.oh_ID AS OHID, o.class_ID AS classID, o.time_Begin AS time, o.location AS room, o.weekDay AS weekDay, c.Start AS startDate, c.End AS endDate FROM office_hour o INNER JOIN classes c ON c.class_ID = o.class_ID WHERE o.class_ID = ".$_POST['select'];
 	$statement = $conn->prepare($cmd);
 	$statement->execute();
 	$return = array();
 	while($result = $statement->fetch()){
-		$return["json"] = json_encode(array('OHID'=>$result['oh_ID'], "time"=>$result['time_Begin'], "location"=>$result['location']));
+		$return[] = array('OHID'=>$result['OHID'], "time"=>$result['time'], "location"=>$result['room'], "weekday"=>$result['weekDay'], "startDate"=>$result['startDate'], "endDate"=>$result['endDate']);
     //echo $result['oh_ID'].$result['time_Begin'].$result['location'];
 	}
   //Do what you need to do with the info. The following are some examples.
