5555253f15177d1f0fdecb92798aad6bec3025c7
Nicholas Pena <np1514@nyu.edu>
2017-02-19 00:01:28 -0500

    minor css stuff???

--- include/header.php
+++ include/header.php
@@ -1,8 +1,29 @@
+<!--
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+  		<div class="container-fluid">
+    		<div class="navbar-header">
+      			<div id = "logo"><..?php include "logo/logo.php" ?></div>
+    		</div>
+    		<ul class="nav navbar-nav navbar-right">
+		      	<li class="active"><a href="#">Home</a></li>
+		      	<li><a href="officehour.php">Office Hour</a></li>
+		      	<li><a href = "myclasses.php">My Classes</a></li>
+		      	<li><a href = "help.php">Help</a></li>
+		      	<li><a id = "profile">Profile</a></li><br>
+    		</ul>
+    		<div id = "subprofile">
+				<div><img alt = "pfp"></div>
+				<div><a href = "setting.php">Setting</a></div>
+				<div><a href = "logOut.php">Logout</a></div>
+			</div>
+  		</div>
+	</nav>
+-->
 
-	<div id = "navbar">
-		<div id = "logo"><?php include "logo/logo.php"?></div>
+	<div id = "navbar" class = "navbar navbar-fixed-top">
+		<div id = "logo"><?php include "logo/logo.php" ?></div>
 		<div class = "navcontainer">
-			<a href = "officehour.php">Office Hour</a><!--add a drop tab there for prof and ta-->
+			<a href = "officehour.php">Office Hour</a>	<!--	add a drop tab there for prof and ta -->
 			<a href = "myclasses.php">My Classes</a>
 			<a href = "help.php">Help</a>
 			<a id = "profile">Profile</a><br>
@@ -13,3 +34,4 @@
 			</div>
 		</div>
 	</div>
+	-->
