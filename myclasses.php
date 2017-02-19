c7b83181ef6667329fc2c4895190ab960547768c
ntc27 <ntc27@cornell.edu>
2017-02-18 19:03:40 -0500

    fucking shit

--- myclasses.php
+++ myclasses.php
@@ -75,9 +75,34 @@
 					return false;
 				}
 			}
+			
 
+		})
+			function getDates(day, startingDate, endingDate){
+				var sDate = new Date(startingDate);
+				var eDate = new Date(endingDate);
+				var startDay = sDate.getDay();
+				var difference = day - startDay;
+				if (difference < 0)
+					difference += 7;
+				var duration = Math.abs(DateBtwn(starting));
+				var remainder = duration%7;
+			}
+			function DateBtwn( date1, date2 ) {
+			  //Get 1 day in milliseconds
+			  var one_day=1000*60*60*24;
 
-		});})
+			  // Convert both dates to milliseconds
+			  var date1_ms = date1.getTime();
+			  var date2_ms = date2.getTime();
+
+			  // Calculate the difference in milliseconds
+			  var difference_ms = date2_ms - date1_ms;
+				
+			  // Convert back to days and return
+			  return Math.round(difference_ms/one_day); 
+			}
+		;})
 		</script>
 	</head>
 	<body>
